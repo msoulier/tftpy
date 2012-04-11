2b879b34d6cbd8539c785eec8ef415f60b928f86
Michael Farrell <micolous+git@gmail.com>
2012-04-11 17:22:03 +0930

    allow running the script on python 3.0 - 3.2 (though unsure of compatibility)

--- tftpy/__init__.py
+++ tftpy/__init__.py
@@ -12,7 +12,7 @@ import sys
 
 # Make sure that this is at least Python 2.3
 verlist = sys.version_info
-if not verlist[0] >= 2 or not verlist[1] >= 3:
+if not (verlist[0] > 2 or (verlist[0] == 2 and verlist[1] >= 3)):
     raise AssertionError, "Requires at least Python 2.3"
 
 from TftpShared import *
