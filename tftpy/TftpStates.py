d96cc63d139e0af28712def504d8fe02cf542823
Lionel Hubaut <lionel.hubaut@tessares.net>
2017-10-03 12:09:47 +0200

    raise: fix descrepancy between python2-3 syntax
    Signed-off-by: Lionel Hubaut <lionel.hubaut@tessares.net>

--- tftpy/TftpStates.py
+++ tftpy/TftpStates.py
@@ -368,7 +368,7 @@ class TftpStateServerRecvWRQ(TftpServerState):
             f = self.context.upload_open(path, self.context)
             if f is None:
                 self.sendError(TftpErrors.AccessViolation)
-                raise TftpException, "Dynamic path %s not permitted" % path
+                raise TftpException("Dynamic path %s not permitted" % path)
             else:
                 self.context.fileobj = f
         else:
