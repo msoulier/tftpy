e116d246852e1d621fb63ebbe1bd4296e8f1c1d6
Michael P. Soulier <msoulier@digitaltorque.ca>
2018-05-18 08:11:40 -0400

    Preparing for 0.6.3 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='tftpy',
-      version='0.6.3_fork',
+      version='0.6.3',
       description='Python TFTP library',
       author='Michael P. Soulier',
       author_email='msoulier@digitaltorque.ca',
