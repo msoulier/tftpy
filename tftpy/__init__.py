5502b2895116db3a7c00f74aedd5daa5b5a851e5
Michael P. Soulier <msoulier@digitaltorque.ca>
2021-10-19 11:55:20 -0400

    Dropping Python 2 support. #127

--- tftpy/__init__.py
+++ tftpy/__init__.py
@@ -13,10 +13,10 @@ directly. The TftpClient and TftpServer classes can be reached through it.
 
 import sys
 
-# Make sure that this is at least Python 2.7
-required_version = (2, 7)
+# Make sure that this is at least Python 3
+required_version = (3, 0)
 if sys.version_info < required_version:
-    raise ImportError("Requires at least Python 2.7")
+    raise ImportError("Requires at least Python 3.0")
 
 from .TftpShared import *
 from . import TftpPacketTypes
