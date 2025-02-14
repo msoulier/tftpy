084fba6bea216155017c3e9e6d5b47d3804a18e4
Michael P. Soulier <msoulier@digitaltorque.ca>
2025-02-14 09:00:32 -0500

    Removing pkg_resources requirement. Closes #145.

--- tftpy/__init__.py
+++ tftpy/__init__.py
@@ -16,20 +16,11 @@ from . import __name__ as pkg_name
 from . import TftpContexts, TftpPacketFactory, TftpPacketTypes, TftpStates
 import sys
 
-import pkg_resources
+from importlib.metadata import version
 
 # Make sure that this is at least Python 3
 required_version = (3, 0)
 if sys.version_info < required_version:
     raise ImportError("Requires at least Python 3.0")
 
-
-def _get_version():
-    try:
-        pkg_version = pkg_resources.get_distribution(pkg_name).version
-    except pkg_resources.DistributionNotFound:
-        pkg_version = None
-    return pkg_version
-
-
-__version__ = _get_version()
+__version__ = version("tftpy")
