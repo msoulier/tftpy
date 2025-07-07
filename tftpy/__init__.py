8236ec33685553a7a7ccdfd5c40105f41904fe21
Michael P. Soulier <msoulier@digitaltorque.ca>
2025-07-07 17:44:45 -0400

    Making use of importlib more tolerant

--- tftpy/__init__.py
+++ tftpy/__init__.py
@@ -16,11 +16,17 @@ from . import __name__ as pkg_name
 from . import TftpContexts, TftpPacketFactory, TftpPacketTypes, TftpStates
 import sys
 
-from importlib.metadata import version
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
 
 # Make sure that this is at least Python 3
 required_version = (3, 8)
 if sys.version_info < required_version:
     raise ImportError("Requires at least Python 3.8")
 
-__version__ = version("tftpy")
+try:
+    __version__ = version("tftpy")
+except PackageNotFoundError:
+    __version__ = '0.0.0-dev'
